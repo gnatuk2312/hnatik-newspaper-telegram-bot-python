a8cd09e4e82032b6b38a737235c548f30c9d3380
Hnat Liashenko <hnat.liashenko@bluepes.com>
2024-03-13 20:22:38 +0200

    add try/except for cron loop in case if user deletes the chat with bot

--- src/cron.py
+++ src/cron.py
@@ -14,20 +14,26 @@ async def send_newspaper_to_all_users(timestamp):
     users = await get_all_users()
 
     for user in users:
-        chat_id = user["chat_id"]
-        first_name = user["first_name"]
+        try:
+            chat_id = user["chat_id"]
+            first_name = user["first_name"]
 
-        await bot.send_message(
-            chat_id, f"Добрий ранок, {first_name}🌅\nОсь твоя ранкова газета 📰"
-        )
+            await bot.send_message(
+                chat_id, f"Добрий ранок, {first_name}🌅\nОсь твоя ранкова газета 📰"
+            )
 
-        weather_message = await get_weather_subscription_message_for_user(user)
-        await bot.send_message(chat_id, weather_message, parse_mode="markdown")
+            weather_message = await get_weather_subscription_message_for_user(user)
+            await bot.send_message(chat_id, weather_message, parse_mode="markdown")
 
-        currency_message = await get_currency_subscription_message_for_user(user)
-        await bot.send_message(chat_id, currency_message, parse_mode="markdown")
+            currency_message = await get_currency_subscription_message_for_user(user)
+            await bot.send_message(chat_id, currency_message, parse_mode="markdown")
 
-        cryptocurrency_message = await get_cryptocurrency_subscription_message_for_user(
-            user
-        )
-        await bot.send_message(chat_id, cryptocurrency_message, parse_mode="markdown")
+            cryptocurrency_message = (
+                await get_cryptocurrency_subscription_message_for_user(user)
+            )
+            await bot.send_message(
+                chat_id, cryptocurrency_message, parse_mode="markdown"
+            )
+        except Exception as error:
+            print(f"Cron >> send_newspaper_to_all_users > {error}")
+            continue
